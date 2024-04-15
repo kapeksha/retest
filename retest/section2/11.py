95993dd0e44dcfc9efe899e5a1a84d183622b2c5
kapeksha <165630326+kapeksha@users.noreply.github.com>
2024-04-15 16:30:32 +0530

    11.py

--- retest/section2/11.py
+++ retest/section2/11.py
@@ -1,11 +1,22 @@
 #11.create decorator function to measure execution time of function
 
 import time
+def decorator(func):
+    def wrap(*args, **kwargs):
+        start_time = time.time()  
+        print(f"Calling {func.__name__} with args: {args}, kwargs: {kwargs}")
+        
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} returned: {result}")
+        end_time = time.time()  
+        time_taken = end_time - start_time  
+        print(f"Function {func.__name__} executed in {time_taken:.4f} seconds.")
+        return result
+    return wrap
 
-def example_function(n):
-    time.sleep(n)
-start_time = time.time()
-example_function(2)
-end_time = time.time()
-execution_time = end_time - start_time
-print(f"Execution time: {execution_time} seconds")
+@decorator
+def multiply_numbers(x, y):
+    return x * y
+
+p=decorator(multiply_numbers)
+p(10, 20) 
